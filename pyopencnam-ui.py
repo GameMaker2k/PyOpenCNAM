99365647ac3d5f4cc6a63ce40a11f0df4add252c
Joshua Thomas Przyborowski <joshua.przyborowski@gmail.com>
2017-12-27 14:38:36 -0600

    Update pyopencnam-ui.py

--- pyopencnam-ui.py
+++ pyopencnam-ui.py
@@ -93,19 +93,16 @@ phonenumber_label = Label(root, text="Phone Number", height=1);
 phonenumber_label.pack(side=TOP, anchor="w");
 phonenumber = Entry(root, textvariable=StringVar(root, value=master_phone_number), width=65);
 phonenumber.pack(side=TOP, anchor="w");
-phonenumber.insert(END, master_phone_number);
 
 accountsid_label = Label(root, text="Account SID", height=1);
 accountsid_label.pack(side=TOP, anchor="w");
 accountsid = Entry(root, textvariable=StringVar(root, value=master_account_sid), width=65);
 accountsid.pack(side=TOP, anchor="w");
-accountsid.insert(END, master_account_sid);
 
 authtoken_label = Label(root, text="Auth Token", height=1);
 authtoken_label.pack(side=TOP, anchor="w");
 authtoken = Entry(root, textvariable=StringVar(root, value=master_auth_token), width=65);
 authtoken.pack(side=TOP, anchor="w");
-authtoken.insert(END, master_auth_token);
 
 service_level = reversed(["standard", "plus"]);
 servicelevel_label = Label(root, text="Service Level", height=1);
